ef45be70ed9bcc853bbb03c722f625f1db968bff
Maryem khaoua <71089834+MaryemKhaoua@users.noreply.github.com>
2022-08-05 19:02:12 +0100

    Add files via upload
    Calculer de factorielle en utilisant loop for

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int factorial(int N)
-{
-    int i, f;
-    f = 1;
-	for (i = 1; i <= N; i++)
-	f = f * i;
-	return f;
-}
 int main()
 {
-    int nbr;
-    printf("veuillez saisire un nombre pour calculer le factoriel : \n");
-	scanf("%d", &nbr);
-	printf("le factoriel est \n %d ! = %d", nbr, factorial(nbr));
+   int Nbr, i, f;
+
+	printf("veuillez saisire un nombre pour calculer le factoriel : \n");
+	scanf("%d", &Nbr);
+	f = 1;
+	for (i = 1; i <= Nbr; i++)
+	f = f * i;
+	printf("le factoriel de %d = %d", Nbr, f);
 	return 0;
 }
